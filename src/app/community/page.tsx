e5f1d94daa634372bef59a23d9792ad71ebc9796
Christopher Ray <christopherray@Mac.attlocal.net>
2025-04-09 18:17:09 -0400

    Fix Vercel build errors: Update community imports to use relative paths

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { FiSearch, FiGrid, FiList, FiMapPin, FiStar } from "react-icons/fi";
 import { communities } from '../../lib/data/communities';
 import type { Community } from '../../lib/data/communities';
-import { getCommunityPath } from "@/lib/utils/formatSlug";
+import { getCommunityPath } from "../../lib/utils/formatSlug";
 
 // Get unique services from all communities
 const allServices = Array.from(
